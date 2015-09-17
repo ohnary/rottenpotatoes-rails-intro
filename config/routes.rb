6a1e0be6afa760ff307eda67e104692f6cb39404
ohnarya <ohnarya@tamu.edu>
2015-09-16 20:18:59 -0700

    change routes.rb to add root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    root 'movies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :movies, path:''
+  resources :movies
   
   # Example resource route with options:
   #   resources :products do
